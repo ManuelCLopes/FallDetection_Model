9992d94b4aa67dec32352d675f94bfbe51ed33b3
ManuelCLopes <manuel_clopes@outlook.com>
2021-12-23 15:43:56 +0000

    dataset_pre_preparation

--- data_prepatation.py
+++ data_prepatation.py
@@ -1,24 +1,47 @@
 import os
 import io
 import numpy as np
+from numpy.ma.core import append
+from scipy.stats import kurtosis, skew
+import csv
+
 
 subject_folders = os.listdir("Sisfall_dataset")
+
+new_csv = [[]]
 for i in subject_folders:
     temp_folder = []
     temp_folder = os.listdir("Sisfall_dataset/" + i)
     number_files = len(temp_folder)
-    #print("\n" + i + ":" + str(number_files))
-
-    final_list = [] 
     for j in temp_folder:
-        #print("\t" + j)
-        if(j == "F15_SA21_R02.txt"):
-            s = io.BytesIO(open("Sisfall_dataset/" + i + "/" + j, 'rb').read().replace(b';',b''))
-            data = np.genfromtxt(s,dtype=int,delimiter=',')
-            print("Max amplitude: " + str(max(data[:, 0])))
-            print("Min amplitude: " + str(min(data[:, 0])))
-            print("Mean amplitude: " + str(np.mean(data[:, 0])))
-            print("Variance: " + str(np.var(data[:, 0])))
-            print("Kurtosis: " + str(kurtosis(data[:, 0])))
-            print("Skewness: " + str(np.mean(data[:, 0])))
+        file = io.BytesIO(open("Sisfall_dataset/" + i + "/" + j, 'rb').read().replace(b';',b''))
+        data = np.genfromtxt(file,dtype=int,delimiter=',')
+        line = []
+        for col in range(len(data.T)):
+            min_ = min(data[:, col])
+            max_ = max(data[:, col])
+            mean_ = np.mean(data[:, col])
+            variance = np.var(data[:, col])
+            k = kurtosis(data[:, col])
+            s = skew(data[:, col])
+            
+            line.append(min_)
+            line.append(max_)
+            line.append(mean_)
+            line.append(variance)
+            line.append(k)
+            line.append(s)
+        
+        if (j[0] == "F"):
+            line.append(1)
+        else:
+            line.append(0)
+        
+
+        new_csv.append(line)
+        file.close()
+
+with open('test.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerows(new_csv)
 
